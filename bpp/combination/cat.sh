7a2393f1511c29929476aab047dce375a9a2f965
basanta33 <basanta.khakurel@selu.edu>
2022-07-04 12:52:30 -0500

    setting up for new analyses

--- bpp/combination/cat.sh
+++
@@ -1,7 +0,0 @@
-for file in don/*.txt 
-do 
-	cp $file ./f.txt  
-	grep -A1 '(A) List of best models (count postP #species SpeciesTree)' ./f.txt > all.txt
-mv $file ./ok 
-mv all.txt $file.txt 
-done
