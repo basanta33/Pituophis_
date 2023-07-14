57b4a19d0f03ce48f26bcb41aa642bfa6d642c08
Basanta Khakurel <basanta.khakurel@gmail.com>
2023-07-14 10:50:19 -0500

    some extra analyses

--- Map.R
+++ Map.R
@@ -2,14 +2,20 @@ library(ggplot2)
 library(readr)
 library(leaflet)
 library(dplyr)
+library(leaflegend)
 library(htmltools)
+
+
 #load the dataframe
 occurences <- read_csv("occurences.csv")
-#mapping occurences
 
+#mapping occurences
+colors1 <- c("black", "red", "green", "blue")
+leafImg1 <- system.file(sprintf("Presentation1.png"), package = "leaflegend")
 subspecies_map <- leaflet(occurences) %>%
   addTiles() %>%
-  addCircleMarkers(~decimalLongitude, ~decimalLatitude, color = ~color, stroke = FALSE, fillOpacity = 1)
+  addCircleMarkers(~decimalLongitude, ~decimalLatitude, color = ~color, stroke = FALSE, fillOpacity = 1) #%>%  
+  #addLegendImage(position = "bottomright", images = leafImg1, labels = colors1)
 
 
 colors <- c("orange", "#3A5795", "#660099", "BADA55")
